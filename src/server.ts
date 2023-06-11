c6924d6e1a56024f3744f5636f381c7456aea24a
Rakib Linux <rakibulinux@gmail.com>
2023-06-11 08:47:57 +0600

    [Done] Create User Interface and Model

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log(`Database Connect Successfully`);
+    await mongoose.connect(config.database_url as string)
+    console.log(`Database Connect Successfully`)
     app.listen(config.port, () => {
       console.log(
         `University Management Auth Service app listening on port ${config.port}`
-      );
-    });
+      )
+    })
   } catch (error) {
-    console.log(`${error}`);
+    console.log(`${error}`)
   }
 }
 
-bootstrap();
+bootstrap()
