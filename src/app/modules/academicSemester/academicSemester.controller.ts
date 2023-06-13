6dcdbacd1e7e4113208e0e308f995ae156559e44
Rakib Linux <rakibulinux@gmail.com>
2023-06-13 10:51:13 +0600

    [Done] Fix Some Error In Academic Semester

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -10,13 +10,13 @@ const createSemester = catchAsync(
     const result = await AcademicSemesterService.createSemister(
       academicSemesterData
     );
-    next();
     sendResponse(res, {
       statusCode: httpStatus.OK,
       success: true,
       message: 'Academic Semester is Created Successfully',
       data: result,
     });
+    next();
   }
 );
 
